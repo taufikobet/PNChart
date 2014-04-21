4c9f44ee2b0218ec6fb60dcae0e756ccdbfb4bff
Taufik Obet <taufik@icehousecorp.com>
2014-04-21 17:38:33 +0700

    Change project to Universal

--- PNChartDemo/PCChartViewController.m
+++ PNChartDemo/PCChartViewController.m
@@ -7,9 +7,15 @@
 //
 
 #import "PCChartViewController.h"
+#import "PNChart.h"
+#import "PNLineChartData.h"
+#import "PNLineChartDataItem.h"
 
 
 @interface PCChartViewController ()
+@property (nonatomic, strong) PNLineChart *lineChart;
+@property (nonatomic, strong) PNBarChart *barChart;
+@property (nonatomic, strong) PNCircleChart *circleChart;
 
 @end
 
@@ -19,18 +25,106 @@
 {
     [super viewDidLoad];
 	
-	// Do any additional setup after loading the view.
+//    self.automaticallyAdjustsScrollViewInsets = YES;
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
+    /*
+    {
+        //Add LineChart
+        UILabel * lineChartLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 90, SCREEN_WIDTH, 30)];
+        lineChartLabel.text = @"Line Chart";
+        lineChartLabel.textColor = PNFreshGreen;
+        lineChartLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:23.0];
+        lineChartLabel.textAlignment = NSTextAlignmentCenter;
+        
+        self.lineChart = [[PNLineChart alloc] initWithFrame:CGRectMake(0, 135.0, SCREEN_WIDTH, SCREEN_HEIGHT)];
+        self.lineChart.backgroundColor = [UIColor clearColor];
+        
+        [self.lineChart setXLabels:@[@"NOV",@"DEC",@"JAN",@"FEB"]];
+        
+        // Line Chart Nr.1
+        NSArray * yData = @[@3, @1, @5, @6];
+        PNLineChartData *data01 = [PNLineChartData new];
+        data01.color = PNFreshGreen;
+        data01.itemCount = self.lineChart.xLabels.count;
+        data01.getData = ^(NSUInteger index) {
+            CGFloat yValue = [[yData objectAtIndex:index] floatValue];
+            return [PNLineChartDataItem dataItemWithY:yValue];
+        };
+        
+        self.lineChart.chartData = @[data01];
+        [self.lineChart strokeChart];
+        
+        self.lineChart.delegate = self;
+        
+        [self.view addSubview:lineChartLabel];
+        [self.view addSubview:self.lineChart];
+        
+        self.title = @"Line Chart";
+    }
+    */
+    
+    {
+        //Add BarChart
+        
+        UILabel * barChartLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 90, SCREEN_WIDTH, 30)];
+        barChartLabel.text = @"Bar Chart";
+        barChartLabel.textColor = PNFreshGreen;
+        barChartLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:23.0];
+        barChartLabel.textAlignment = NSTextAlignmentCenter;
+        
+        self.barChart = [[PNBarChart alloc] initWithFrame:self.view.frame];
+        self.barChart.backgroundColor = [UIColor clearColor];
+        self.barChart.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+        [self.barChart setXLabels:@[@"SEP 1",@"SEP 2",@"SEP 3",@"SEP 4",@"SEP 5",@"SEP 6",@"SEP 7"]];
+        [self.barChart setYValues:@[@1,@24,@12,@18,@30,@10,@21]];
+        [self.barChart setStrokeColors:@[PNGreen,PNGreen,PNRed,PNGreen,PNGreen,PNYellow,PNGreen]];
+        
+        [self.view addSubview:barChartLabel];
+        [self.view addSubview:self.barChart];
+        
+        self.title = @"Bar Chart";
+    }
+
+    /*
+    {
+        
+        //Add CircleChart
+        
+        
+        UILabel * circleChartLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 90, SCREEN_WIDTH, 30)];
+        circleChartLabel.text = @"Circle Chart";
+        circleChartLabel.textColor = PNFreshGreen;
+        circleChartLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:23.0];
+        circleChartLabel.textAlignment = NSTextAlignmentCenter;
+        
+        self.circleChart = [[PNCircleChart alloc] initWithFrame:CGRectMake(0, 80.0, SCREEN_WIDTH, SCREEN_HEIGHT) andTotal:[NSNumber numberWithInt:100] andCurrent:[NSNumber numberWithInt:60]];
+        self.circleChart.backgroundColor = [UIColor clearColor];
+        [self.circleChart setStrokeColor:PNGreen];
+        [self.circleChart strokeChart];
+        
+        [self.view addSubview:circleChartLabel];
+        [self.view addSubview:self.circleChart];
+        self.title = @"Circle Chart";
+        
+    }
+    */
 }
 
-- (void)viewWillAppear:(BOOL)animated
+-(void)viewDidAppear:(BOOL)animated
 {
-    [super viewWillAppear:animated];
+    [super viewDidAppear:animated];
+    
+    [self.barChart strokeChart];
 }
 
-- (void)didReceiveMemoryWarning
+-(void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self.barChart strokeChart];
+}
+
+-(void)viewDidLayoutSubviews
+{
 }
 
 @end
--- PNChartDemo/PCChartsTableViewController.m
+++ PNChartDemo/PCChartsTableViewController.m
@@ -7,12 +7,8 @@
 //
 
 #import "PCChartsTableViewController.h"
-#import "PNChart.h"
-#import "PNLineChartData.h"
-#import "PNLineChartDataItem.h"
 
 @interface PCChartsTableViewController ()
-
 @end
 
 @implementation PCChartsTableViewController
@@ -29,7 +25,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
  
@@ -50,84 +46,8 @@
     // Pass the selected object to the new view controller.
     
     UIViewController * viewController = [segue destinationViewController];
-    
-    if ([segue.identifier isEqualToString:@"lineChart"]) {
-        
-        //Add LineChart
-        UILabel * lineChartLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 90, SCREEN_WIDTH, 30)];
-        lineChartLabel.text = @"Line Chart";
-        lineChartLabel.textColor = PNFreshGreen;
-        lineChartLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:23.0];
-        lineChartLabel.textAlignment = NSTextAlignmentCenter;
-        
-        PNLineChart * lineChart = [[PNLineChart alloc] initWithFrame:CGRectMake(0, 135.0, SCREEN_WIDTH, 200.0)];
-        lineChart.backgroundColor = [UIColor clearColor];
-        
-        [lineChart setXLabels:@[@"NOV",@"DEC",@"JAN",@"FEB"]];
-
-        // Line Chart Nr.1
-        NSArray * yData = @[@3, @1, @5, @6];
-        PNLineChartData *data01 = [PNLineChartData new];
-        data01.color = PNFreshGreen;
-        data01.itemCount = lineChart.xLabels.count;
-        data01.getData = ^(NSUInteger index) {
-            CGFloat yValue = [[yData objectAtIndex:index] floatValue];
-            return [PNLineChartDataItem dataItemWithY:yValue];
-        };
-
-        lineChart.chartData = @[data01];
-        [lineChart strokeChart];
-        
-        lineChart.delegate = self;
-        
-        [viewController.view addSubview:lineChartLabel];
-        [viewController.view addSubview:lineChart];
-        
-        viewController.title = @"Line Chart";
-        
-    }else if ([segue.identifier isEqualToString:@"barChart"])
-    {
-        //Add BarChart
-        
-        UILabel * barChartLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 90, SCREEN_WIDTH, 30)];
-        barChartLabel.text = @"Bar Chart";
-        barChartLabel.textColor = PNFreshGreen;
-        barChartLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:23.0];
-        barChartLabel.textAlignment = NSTextAlignmentCenter;
-        
-        PNBarChart * barChart = [[PNBarChart alloc] initWithFrame:CGRectMake(0, 135.0, SCREEN_WIDTH, 200.0)];
-        barChart.backgroundColor = [UIColor clearColor];
-        [barChart setXLabels:@[@"SEP 1",@"SEP 2",@"SEP 3",@"SEP 4",@"SEP 5",@"SEP 6",@"SEP 7"]];
-        [barChart setYValues:@[@1,@24,@12,@18,@30,@10,@21]];
-        [barChart setStrokeColors:@[PNGreen,PNGreen,PNRed,PNGreen,PNGreen,PNYellow,PNGreen]];
-        [barChart strokeChart];
-        
-        [viewController.view addSubview:barChartLabel];
-        [viewController.view addSubview:barChart];
-        
-        viewController.title = @"Bar Chart";
-    }else if ([segue.identifier isEqualToString:@"circleChart"])
-    {
-        
-        //Add CircleChart
-        
-        
-        UILabel * circleChartLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 90, SCREEN_WIDTH, 30)];
-        circleChartLabel.text = @"Circle Chart";
-        circleChartLabel.textColor = PNFreshGreen;
-        circleChartLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:23.0];
-        circleChartLabel.textAlignment = NSTextAlignmentCenter;
-        
-        PNCircleChart * circleChart = [[PNCircleChart alloc] initWithFrame:CGRectMake(0, 80.0, SCREEN_WIDTH, 100.0) andTotal:[NSNumber numberWithInt:100] andCurrent:[NSNumber numberWithInt:60]];
-        circleChart.backgroundColor = [UIColor clearColor];
-        [circleChart setStrokeColor:PNGreen];
-        [circleChart strokeChart];
-        
-        [viewController.view addSubview:circleChartLabel];
-        [viewController.view addSubview:circleChart];
-        viewController.title = @"Circle Chart";
-        
-    }
+    viewController.automaticallyAdjustsScrollViewInsets = YES;
+    viewController.edgesForExtendedLayout = UIRectEdgeNone;
     
 }
 
